ea773f6e9932045f5d5ef477292a95f9909c3ccb
Joshua Kim <joshkim23@gmail.com>
2021-04-06 12:44:03 -0700

    cleaned up quick sort, added base case step adding so the single element quick sort calls are sorted. Probably still gonna try to add another color for the pivot so it's clearer where the pivot is

--- src/Algorithms/quickSort.ts
+++ src/Algorithms/quickSort.ts
@@ -3,59 +3,57 @@ import { Tracker } from '../IterationTrackerInterface';
 
 export const QuickSortMain = (list:Array<number>):Tracker => {
     let tracker:Tracker = createNewTracker([...list]);
-    QuickSort(list, tracker, 0, list.length-1);
+    let sortedIndicesArray:Array<number> = [];
+    QuickSort(list, tracker, sortedIndicesArray, 0, list.length-1);
 
     addStepToTracker(tracker, list, [], getSortedIndicesArray(list.length), [], 'sorted list');
     return tracker;
 }
 
 
-function QuickSort(list:Array<number>, tracker:Tracker, left:number, right:number) {
+function QuickSort(list:Array<number>, tracker:Tracker, sortedIndicesArray:Array<number>, left:number, right:number) {
     if (left < right) {
-        let j = partition(list, tracker, left, right);
-        addStepToTracker(tracker, [...list], [], [j], [], 'post partition');
-        QuickSort(list, tracker, left, j-1);
-        QuickSort(list, tracker, j+1, right);
+        let j = partition(list, tracker, sortedIndicesArray, left, right);
+        sortedIndicesArray.push(j);
+        addStepToTracker(tracker, [...list], [], [...sortedIndicesArray], [], 'post partition');
+        QuickSort(list, tracker, sortedIndicesArray, left, j-1);
+        QuickSort(list, tracker, sortedIndicesArray, j+1, right);
+    } else {
+        sortedIndicesArray.push(left);
+        addStepToTracker(tracker, [...list], [], [...sortedIndicesArray], [], 'base case, only one element - add to sorted');
     }
 }
 
-function partition(list:Array<number>, tracker:Tracker, start:number, end:number):number {
-    // let pivot = list[end];
-    // let leftPointer = start;
-
-    // for (let j=start; j<end; j++) {
-    //     // addStepToTracker(tracker, [...list], [leftPointer, j], [], [], 'comparing values to pivot');
-    //     if (list[j] < pivot) {
-    //         swap(list, leftPointer, j);
-    //         addStepToTracker(tracker, [...list], [], [], [leftPointer, j], 'swapped');
-
-    //         leftPointer++;
-    //     }
-    // }
-    // swap(list, end, leftPointer);
-    // addStepToTracker(tracker, [...list], [], [leftPointer], [end, leftPointer], 'swapped');
-    //     return leftPointer;
-
-    let pivot = list[start];
-    let i = start+1;
+function partition(list:Array<number>, tracker:Tracker, sortedIndicesArray:Array<number>, start:number, end:number):number {
+    const pivotIndex = start; // set pivot to first index of the list 
+    let pivot = list[pivotIndex];
+    addStepToTracker(tracker, [...list], [pivotIndex], [...sortedIndicesArray], [], `adding pivot to tracker. Pivot VALUE: ${pivot}`);
+    let i = pivotIndex+1;
     let j = end;
-    // console.log(' *********************** START/END PARTITION ******************')
-    // console.log('pivot: ', pivot);
-    while (i < j) {
-        while (list[i] < pivot) i++;
-        while (list[j] > pivot) j--;
 
-        console.log('i: ', i, 'j: ', j);
+    while (i < j) {
+        while (list[i] < pivot) {
+            addStepToTracker(tracker, [...list], [pivotIndex, i], [...sortedIndicesArray], [], 'incrementing i');
+            i++;
+            if (list[i] > pivot) addStepToTracker(tracker, [...list], [pivotIndex], [...sortedIndicesArray], [i], 'next i value breaks while loop, value at i is GREATER than pivot');
+        }
+        while (list[j] > pivot) {
+            addStepToTracker(tracker, [...list], [pivotIndex, j], [...sortedIndicesArray], [], 'incrementing j');
+            j--;
+            if (list[j] < pivot) addStepToTracker(tracker, [...list], [pivotIndex], [...sortedIndicesArray], [j], 'next j value breaks while loop, value at j is LESS than pivot');
+
+        }
+
+        addStepToTracker(tracker, [...list], [pivotIndex, i, j], [...sortedIndicesArray], [], 'after i and j increment')
+
+
         if (i<j) {
             swap(list, i, j);
-            console.log('indexes swapped, i: ', i, 'j: ', j);
-            console.log('values swapped: ', list[i], list[j]);
-            console.log('updated list: ', list);
+            addStepToTracker(tracker, [...list], [pivotIndex], [...sortedIndicesArray], [i, j], 'swapped i and j');
         }
-        console.log('exit while loop, nothing to swap');
     }
     if (list[j] < pivot) {
-        swap(list, j, start);
+        swap(list, j, pivotIndex);
         console.log('list after swapping pivot with j: ', list);
     }
     return j;
